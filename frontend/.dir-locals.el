2a79f468a5fd12991e78115712ad9a20884a10d5
Compro-Prasad <comproprasad@gmail.com>
2019-06-15 19:37:56 +0530

    Added frontend/.dir-locals.el

---
+++ frontend/.dir-locals.el
@@ -0,0 +1,11 @@
+(
+  (web-mode . ((web-mode-attr-indent-offset . 2)
+                (web-mode-markup-indent-offset . 2)
+                (web-mode-code-indent-offset . 2)
+                (web-mode-css-indent-offset . 2)
+                (web-mode-part-padding . 2)
+                (web-mode-style-padding . 0)
+                (web-mode-block-padding . 2)
+                (web-mode-script-padding . 0))
+    )
+  )
